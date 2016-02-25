c87b5bcde979bd56177a5070df8a768e3b7b4321
Stephen Sheridan <stesher@gmail.com>
2016-02-25 21:52:53 +0000

    Added files via upload
    First commit

---
+++ simple-caesar.py
@@ -0,0 +1,28 @@
+import sys
+
+def echo(s):
+    sys.stdout.write(s)
+
+plain = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+
+print "Plain text"
+for x in plain:
+    echo(x), echo(' ')
+print ''
+print ''
+print "Cipher text"
+for x in plain:
+    echo(chr(ord(x)-2)), echo(' ')
+print ''
+print ''
+
+plainText = "Hello Jack this is a secret message Love You Lots"
+
+upper = plainText.upper()
+
+print "Secret message"
+for c in upper:
+    if ord(c) == 32:
+        echo(' ')
+    else:
+        echo(chr(ord(c)-2))
